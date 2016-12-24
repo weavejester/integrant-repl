6c3fb901d4194c384756f7e7963a1265e1d1af57
James Reeves <jreeves@weavejester.com>
2016-12-24 20:16:23 +0000

    Add Integrant and tools.namespace dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.namespace "0.2.11"]
+                 [integrant "0.1.5"]])
