77900209d1b08abea6dd120b86ec46e341e7e148
James Reeves <jreeves@weavejester.com>
2016-12-24 18:46:43 +0000

    Initial commit

---
+++ src/integrant/repl.clj
@@ -0,0 +1,6 @@
+(ns integrant.repl)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/integrant/repl_test.clj
@@ -0,0 +1,7 @@
+(ns integrant.repl-test
+  (:require [clojure.test :refer :all]
+            [integrant.repl :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
