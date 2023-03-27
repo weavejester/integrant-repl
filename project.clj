b5ffa34a0c18e15d0b5c94d8a4f7358c29689b22
Mathieu Lirzin <mthl@reuz.fr>
2023-03-27 22:36:51 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/weavejester/integrant-repl"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.namespace "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.namespace "1.4.4"]
                  [integrant "0.8.0"]])
