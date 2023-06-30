a8d926be6a5b45f57719bd46b1ebac840b75e487
James Reeves <jreeves@weavejester.com>
2023-06-30 16:45:20 +0100

    Update dependencies (fixes #25)

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/weavejester/integrant-repl"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.namespace "1.0.0"]
-                 [integrant "0.8.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.namespace "1.4.4"]
+                 [integrant "0.8.1"]])
