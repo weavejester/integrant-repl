95c229c561abe8f6795b4f1c43e8dd5238c6b99b
James Reeves <jreeves@weavejester.com>
2025-09-10 21:39:02 +0100

    Add basic test

--- test/integrant/repl_test.clj
+++ test/integrant/repl_test.clj
@@ -1,6 +1,14 @@
 (ns integrant.repl-test
-  (:require [clojure.test :refer [deftest is testing]]))
+  (:require [clojure.test :refer [deftest is]]
+            [integrant.core :as ig]
+            [integrant.repl :as r]
+            [integrant.repl.state :as rs]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defmethod ig/init-key ::a [_ {:keys [x]}]
+  {:x (inc x)})
+
+(deftest system-test
+  (r/set-prep! (constantly {::a {:x 1}}))
+  (r/go)
+  (is (= {::a {:x 1}} rs/config))
+  (is (= {::a {:x 2}} rs/system)))
